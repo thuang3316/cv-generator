c048f9898d7d9687fe7fe175626c11f871928336
thuang3316 <249622692+thuang3316@users.noreply.github.com>
2026-01-18 10:28:02 -0500

    add resume preview

---
+++ src/components/resume.jsx
@@ -0,0 +1,43 @@
+export function Resume({generalInfoData, eduList}) {
+    const form = generalInfoData;
+
+    return (
+        <div className="resume">
+            <header className="generalInfo">
+                <h1 className="name">{form.firstName + ' ' + form.lastName}</h1>
+                <p className="address">{form.address}</p>
+                <div className="contactInfo">
+                    <div className="email">{form.email}</div>
+                    <div className="phone">{form.phone}</div>
+                </div>
+            </header>
+            <section className="education">
+                <h2>Education</h2>
+                <hr />
+                <ul>
+                    {eduList.map((school) => {
+                        return (
+                            <li key={school.id}>
+                                <div className="schoolInfo">
+                                    <div className="schoolName">{school.schoolName}</div>
+                                    <div className="schoolLocation">{school.schoolLocation}</div>
+                                </div>
+                                <div className="majorInfo">
+                                    <div className="majorName">{school.majorName}</div>
+                                    <div className="duration">{school.start} {school.end}</div>
+                                </div>
+                            </li>
+                        )
+                    })}
+                </ul>
+            </section>
+            <section className="projects">
+                <h2>Projects</h2>
+                <hr />
+                <ul>
+
+                </ul>
+            </section>
+        </div>
+    )
+}
